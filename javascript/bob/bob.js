a894154d7f592099ff3801f083d1135336c902e8
Tom <tomhendersonfl@gmail.com>
2016-05-17 09:53:26 -0600

    initialize exercism repo

---
+++ javascript/bob/bob.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+
+if (input.substring(input.length) = "?") {
+  return "Sure."
+} else {
+  if (input.substring(input.length) = "!") {
+    return "Whoa, chill out!"
+  } else {
+
+  }
+}
+
+};
+
+module.exports = Bob;
